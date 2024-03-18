dc42f13dcc2d15f65d41bcd625deed1b80dc2638
Xoo <1836490825@qq.com>
2024-03-18 14:02:41 +0800

    自定义结构获取作用域插槽数据

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -1,7 +1,29 @@
 <template>
   <div class="container">
     <div class="app-container">
-      <el-tree :data="depts" :props="defaultProps" default-expand-all />
+      <!-- v-slot只能给template使用 -->
+      <el-tree :data="depts" :props="defaultProps" default-expand-all>
+        <template v-slot="{data}">
+          <!-- 节点结构 -->
+          <el-row style="width: 100%; height: 40px;" type="flex" justify="space-between" align="middle">
+            <el-col>{{ data.name }}</el-col>
+            <el-col :span="4">
+              <span class="tree-manger">{{ data.managerName }}</span>
+              <!-- 下拉菜单 -->
+              <el-dropdown>
+                <span>
+                  操作<i class="el-icon-arrow-down el-icon--right" />
+                </span>
+                <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item>添加子部门</el-dropdown-item>
+                  <el-dropdown-item>编辑部门</el-dropdown-item>
+                  <el-dropdown-item>删除</el-dropdown-item>
+                </el-dropdown-menu>
+              </el-dropdown>
+            </el-col>
+          </el-row>
+        </template>
+      </el-tree>
     </div>
   </div>
 </template>
@@ -13,15 +35,16 @@ export default {
     return {
       depts: [
         {
-          label: '传智教育',
+          name: '传智教育',
+          managerName: '管理员',
           children: [
-            { label: '总裁办' },
-            { label: '行政部', children: [{ label: '行政部子部门' }] }
+            { name: '总裁办', managerName: '张三' },
+            { name: '行政部', managerName: '里斯', children: [{ name: '行政部子部门', managerName: '管理员' }] }
           ]
         }
       ], // 数据属性
       defaultProps: {
-        label: 'label', // 要显示的字段名
+        label: 'name', // 要显示的字段名
         children: 'children'
       }
     }
@@ -29,9 +52,15 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .app-container {
   padding: 30px 140px;
   font-size: 14px;
+
+  & .tree-manger {
+    width: 50px;
+    display: inline-block;
+    margin: 10px;
+  }
 }
 </style>
