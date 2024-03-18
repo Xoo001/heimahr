68dea9f969bffa2963eb0966bd090268097ab9e1
Xoo <1836490825@qq.com>
2024-03-18 10:45:28 +0800

    树形结构的应用

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -1,17 +1,37 @@
 <template>
   <div class="container">
     <div class="app-container">
-      组织架构
+      <el-tree :data="depts" :props="defaultProps" default-expand-all />
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Department'
+  name: 'Department',
+  data() {
+    return {
+      depts: [
+        {
+          label: '传智教育',
+          children: [
+            { label: '总裁办' },
+            { label: '行政部', children: [{ label: '行政部子部门' }] }
+          ]
+        }
+      ], // 数据属性
+      defaultProps: {
+        label: 'label', // 要显示的字段名
+        children: 'children'
+      }
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.app-container {
+  padding: 30px 140px;
+  font-size: 14px;
+}
 </style>
