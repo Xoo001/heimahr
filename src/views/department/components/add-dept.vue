8976ae061b308d46835fac60e800840a3d98d5ae
Xoo <1836490825@qq.com>
2024-03-25 15:59:32 +0800

    fix:添加子部门组件的创建

---
+++ src/views/department/components/add-dept.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-dialog title="新增部门" :visible="showDialog" @close="closeDialog">
+    123
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    showDialog: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    closeDialog() {
+      // 改变父级组件的数据
+      this.$emit('update:show-dialog', false)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="app-container">
       <!-- v-slot只能给template使用 -->
-      <el-tree :data="depts" :props="defaultProps" default-expand-all>
+      <el-tree :data="depts" :props="defaultProps" default-expand-all :expand-on-click-node="false">
         <template v-slot="{data}">
           <!-- 节点结构 -->
           <el-row style="width: 100%; height: 40px;" type="flex" justify="space-between" align="middle">
@@ -10,14 +10,14 @@
             <el-col :span="6">
               <span class="tree-manger">{{ data.managerName }}</span>
               <!-- 下拉菜单 -->
-              <el-dropdown>
+              <el-dropdown @command="operateDept">
                 <span>
                   操作<i class="el-icon-arrow-down el-icon--right" />
                 </span>
                 <el-dropdown-menu slot="dropdown">
-                  <el-dropdown-item>添加子部门</el-dropdown-item>
-                  <el-dropdown-item>编辑部门</el-dropdown-item>
-                  <el-dropdown-item>删除</el-dropdown-item>
+                  <el-dropdown-item command="add">添加子部门</el-dropdown-item>
+                  <el-dropdown-item command="edit">编辑部门</el-dropdown-item>
+                  <el-dropdown-item command="delete">删除</el-dropdown-item>
                 </el-dropdown-menu>
               </el-dropdown>
             </el-col>
@@ -25,15 +25,20 @@
         </template>
       </el-tree>
     </div>
+    <!-- 弹出层Dialog -->
+    <AddDept :show-dialog.sync="showDialog" />
   </div>
 </template>
 
 <script>
 import { getDepartment } from '@/api/department'
 import { transListToTreeData } from '@/utils'
+import AddDept from './components/add-dept.vue'
 
 export default {
   name: 'Department',
+
+  components: { AddDept },
 
   data() {
     return {
@@ -41,7 +46,8 @@ export default {
       defaultProps: {
         label: 'name', // 要显示的字段名
         children: 'children'
-      }
+      },
+      showDialog: false
     }
   },
 
@@ -50,12 +56,22 @@ export default {
   },
 
   methods: {
+    // 获取数据
     async getDepartment() {
       const res = await getDepartment()
       // this.depts = res
-      console.log(res)
+      // console.log(res)
       // 树形结构 递归
       this.depts = transListToTreeData(res, 0)
+    },
+
+    // 操作部门的方法
+    operateDept(type) {
+      console.log(type)
+      // 添加子部门
+      if (type === 'add') {
+        this.showDialog = true
+      }
     }
   }
 }
