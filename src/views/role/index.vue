480761d38a7993ff2364dbc38e957ca49fe212d7
Xoo <1836490825@qq.com>
2024-04-16 11:12:39 +0800

    完成角色页面的结构搭建

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -1,7 +1,25 @@
 <template>
   <div class="container">
     <div class="app-container">
-      角色管理
+      <!-- 角色管理 -->
+      <div class="role-operate">
+        <el-button type="primary" size="mini">添加角色</el-button>
+      </div>
+      <!-- 表格组件 -->
+      <el-table border style="width: 100%;">
+        <el-table-column width="50" label="序号" />
+        <el-table-column width="200" label="角色" />
+        <el-table-column width="200" label="启用" />
+        <el-table-column label="描述" />
+        <el-table-column label="操作" />
+      </el-table>
+      <!-- 分页 -->
+      <el-row type="flex" justify="center" style="height: 80px;" align="middle">
+        <el-pagination
+          layout="prev, pager, next"
+          :total="50"
+        />
+      </el-row>
     </div>
   </div>
 </template>
@@ -13,6 +31,11 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.app-container{
+  padding: 20px;
+  .role-operate {
+    margin-bottom: 10px;
+  }
+}
 </style>
