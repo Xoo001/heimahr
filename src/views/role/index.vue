a7632675ecee3a8fb4cc82e426050982c7ecf2f3
Xoo <1836490825@qq.com>
2024-04-23 09:56:07 +0800

    完成角色管理的分页管理

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -9,15 +9,29 @@
       <el-table :data="tableDate" border style="width: 100%;">
         <el-table-column prop="id" width="50" label="序号" />
         <el-table-column prop="name" width="200" label="角色" />
-        <el-table-column prop="state" width="200" label="启用" />
+        <el-table-column prop="state" width="200" label="启用">
+          <!-- 自定义插槽 -->
+          <template v-slot="{row}">
+            <span>{{ row.state === 1 ? '已启用' : '未启用' }}</span>
+          </template>
+        </el-table-column>
         <el-table-column prop="description" label="描述" />
-        <el-table-column label="操作" />
+        <el-table-column label="操作">
+          <template v-slot="">
+            <el-button type="text">分配权限</el-button>
+            <el-button type="text">编辑</el-button>
+            <el-button type="text">删除</el-button>
+          </template>
+        </el-table-column>
       </el-table>
       <!-- 分页 -->
       <el-row type="flex" justify="center" style="height: 80px;" align="middle">
         <el-pagination
+          :page-size="pagesize"
+          :current-page="page"
           layout="prev, pager, next"
-          :total="50"
+          :total="total"
+          @current-change="changePage"
         />
       </el-row>
     </div>
@@ -32,7 +46,8 @@ export default {
     return {
       page: 1, // 当前页码数
       pagesize: 10, // 当前页面需要的数据条数
-      tableDate: [] // 角色列表数据
+      tableDate: [], // 角色列表数据
+      total: 0 // 总数
     }
   },
   created() {
@@ -41,9 +56,15 @@ export default {
   methods: {
     // 调用角色列表数据
     async getRoleList() {
-      const result = await getRoleList()
+      const result = await getRoleList(this.page, this.pagesize)
       console.log(result)
       this.tableDate = result.rows
+      this.total = result.total
+    },
+    // 切换分页请求新的数据
+    changePage(newPage) {
+      this.page = newPage
+      this.getRoleList()
     }
   }
 
