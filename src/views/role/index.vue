e0220ae88d5421d8189f11b08aad73d01e1ff468
Xoo <1836490825@qq.com>
2024-04-24 16:27:05 +0800

    完成编辑功能的缓存数据

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -8,19 +8,37 @@
       <!-- 表格组件 -->
       <el-table :data="tableDate" border style="width: 100%;">
         <el-table-column prop="id" width="50" label="序号" />
-        <el-table-column prop="name" width="200" label="角色" />
+        <el-table-column prop="name" width="200" label="角色">
+          <template v-slot="{row}">
+            <!-- 条件判断 -->
+            <el-input v-if="row.isEdit" v-model="row.editRow.name" size="mini" />
+            <span v-else>{{ row.name }}</span>
+          </template>
+        </el-table-column>
         <el-table-column prop="state" width="200" label="启用">
           <!-- 自定义插槽 -->
           <template v-slot="{row}">
-            <span>{{ row.state === 1 ? '已启用' : '未启用' }}</span>
+            <el-switch v-if="row.isEdit" v-model="row.editRow.state" :active-value="1" :inactive-value="0" />
+            <span v-else>{{ row.state === 1 ? '已启用' : '未启用' }}</span>
           </template>
         </el-table-column>
-        <el-table-column prop="description" label="描述" />
+        <el-table-column prop="description" label="描述">
+          <template v-slot="{row}">
+            <el-input v-if="row.isEdit" v-model="row.editRow.description" type="textarea" />
+            <span v-else>{{ row.description }}</span>
+          </template>
+        </el-table-column>
         <el-table-column label="操作">
-          <template v-slot="">
-            <el-button type="text">分配权限</el-button>
-            <el-button type="text">编辑</el-button>
-            <el-button type="text">删除</el-button>
+          <template v-slot="{row}">
+            <div v-if="row.isEdit">
+              <el-button type="primary" size="mini">确认</el-button>
+              <el-button size="mini" @click="callEdit(row)">取消</el-button>
+            </div>
+            <div v-else>
+              <el-button type="text">分配权限</el-button>
+              <el-button type="text" @click="btnEditRow(row)">编辑</el-button>
+              <el-button type="text">删除</el-button>
+            </div>
           </template>
         </el-table-column>
       </el-table>
@@ -54,7 +72,7 @@ export default {
       pagesize: 5, // 当前页面需要的数据条数
       tableDate: [], // 角色列表数据
       total: 0, // 总数
-      showDialog: true // 对话框显示隐藏
+      showDialog: false // 对话框显示隐藏
     }
   },
   created() {
@@ -64,14 +82,42 @@ export default {
     // 调用角色列表数据
     async getRoleList() {
       const result = await getRoleList(this.page, this.pagesize)
-      console.log(result)
+      // console.log(result)
       this.tableDate = result.rows
       this.total = result.total
+      // 针对每一行数据添加一个标记
+      this.tableDate.forEach(item => {
+        // item.isEdit = false // 添加一个属性 初始值为false
+        // 数据响应式的问题 数据变化 试图更新
+        // 添加的动态属性 不具备响应式的特点
+        // this.$set(目标对象，属性名称，初始值)可以针对目标对象 添加的属性 添加响应式
+        this.$set(item, 'isEdit', false)
+        // 缓存数据
+        this.$set(item, 'editRow', {
+          name: item.name,
+          description: item.description,
+          state: item.state
+        })
+      })
     },
     // 切换分页请求新的数据
     changePage(newPage) {
       this.page = newPage
       this.getRoleList()
+    },
+    // 编辑方法
+    btnEditRow(row) {
+      console.log(row)
+      // 显示编辑内容
+      row.isEdit = true
+      // 更新缓存数据
+      row.editRow.name = row.name
+      row.editRow.description = row.description
+      row.editRow.state = row.state
+    },
+    // 取消编辑
+    callEdit(row) {
+      row.isEdit = false
     }
   }
 
