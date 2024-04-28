4c89146acc0e9954302928a6ff2270d7367a7ba0
Xoo <1836490825@qq.com>
2024-04-28 11:23:45 +0800

    完成分页结构

--- src/views/employee/index.vue
+++ src/views/employee/index.vue
@@ -13,7 +13,27 @@
           <el-button size="mini">excel导出</el-button>
         </el-row>
         <!-- 表格组件 -->
+        <el-table>
+          <el-table-column type="selection" />
+          <el-table-column label="头像" align="center" />
+          <el-table-column label="姓名" />
+          <el-table-column label="手机号" sortable />
+          <el-table-column label="工号" sortable />
+          <el-table-column label="聘用形式" />
+          <el-table-column label="部门" />
+          <el-table-column label="入职时间" sortable />
+          <el-table-column label="操作" width="180px">
+            <template>
+              <el-button type="text">查看</el-button>
+              <el-button type="text">角色</el-button>
+              <el-button type="text">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
         <!-- 分页 -->
+        <el-row style="height: 60px;" type="flex" justify="end" align="middle">
+          <el-pagination layout="total, prev, pager, next" :total="50" />
+        </el-row>
       </div>
     </div>
   </div>
